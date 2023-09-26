8703495f56708132f3a43b9229f7eae5aae34955
Sebastian Perezc Contreras <60678340+Sebastian12111990@users.noreply.github.com>
2023-09-26 01:32:57 -0300

    v1

---
+++ src/main/java/com/GestorTareaApi/app/GestorTareaApiApplication.java
@@ -0,0 +1,13 @@
+package com.GestorTareaApi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorTareaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorTareaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GestorTareaApi/app/GestorTareaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestorTareaApi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTareaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
